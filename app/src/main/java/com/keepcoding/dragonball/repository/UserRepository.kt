a38a2e62d8c8dd0550eadbf7bb51987b1da0f64d
Sergi Gracia <sergiogj1990@gmail.com>
2025-02-27 19:38:29 +0100

    Add rest call for login

--- app/src/main/java/com/keepcoding/dragonball/repository/UserRepository.kt
+++ app/src/main/java/com/keepcoding/dragonball/repository/UserRepository.kt
@@ -1,9 +1,14 @@
 package com.keepcoding.dragonball.repository
 
+import okhttp3.Credentials
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.internal.EMPTY_REQUEST
 import org.jetbrains.annotations.VisibleForTesting
-import kotlin.random.Random
 
 class UserRepository {
+    private val BASE_URL = "https://dragonball.keepcoding.education/api/"
 
     companion object {
         private var token = ""
@@ -14,9 +19,24 @@ class UserRepository {
         data class Error(val message: String) : LoginResponse()
     }
 
+    private val client = OkHttpClient()
+    private val url = "${BASE_URL}auth/login"
+
     fun login(user: String, password: String): LoginResponse {
-        return if (Random.nextBoolean()) {
-            token = "eyJraWQiOiJwcml2YXRlIiwiYWxnIjoiSFMyNTYiLCJ0eXAiOiJKV1QifQ.eyJpZGVudGlmeSI6IjdBQjhBQzRELUFEOEYtNEFDRS1BQTQ1LTIxRTg0QUU4QkJFNyIsImVtYWlsIjoiYmVqbEBrZWVwY29kaW5nLmVzIiwiZXhwaXJhdGlvbiI6NjQwOTIyMTEyMDB9.Dxxy91hTVz3RTF7w1YVTJ7O9g71odRcqgD00gspm30s"
+        val credentials = Credentials.basic(user, password)
+
+        val empty: RequestBody = EMPTY_REQUEST
+        val request = Request.Builder()
+            .url(url)
+            .post(EMPTY_REQUEST)
+            .addHeader("Authorization", credentials)
+            .build()
+
+        val call = client.newCall(request)
+        val response = call.execute()
+
+        return if (response.isSuccessful) {
+            token = response.body!!.string()
             LoginResponse.Success
         } else {
             LoginResponse.Error("501")
